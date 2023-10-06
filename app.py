4b6712ef7912b4c0d6926afa70db9ba97e4d1f3d
divy-a <121391127+divy-a@users.noreply.github.com>
2023-10-06 14:35:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-
 from flask import Flask, request, render_template
 import json
-import traceback
 from yt_dlp import YoutubeDL
 
 app = Flask(__name__)
@@ -28,23 +26,26 @@ def tabulate():
 
         for i, url in enumerate(url_values):
             try:
-                json_array += [{**{'s_no': i+1,'Status': 'OK'}, **info(url, ydl)}]
+                json_array += [{**{'s_no': i+1,
+                                   'Status': 'OK'}, **info(url, ydl)}]
             except Exception as e:
-                json_array += [{'s_no': i+1, 'Status': f'ERROR : {str(e.__context__)}'}]
+                json_array += [{'s_no': i+1,
+                                'Status': f'ERROR : {str(e.__context__)}'}]
 
         return json.dumps(json_array), 200
 
     except Exception as e:
         return {'error': str(e)}, 400
-  
-  
-def info(url : str, ydl:  YoutubeDL) -> dict:
+
+
+def info(url: str, ydl:  YoutubeDL) -> dict:
     info = ydl.extract_info(url, download=False)
-        
+
     if info != None:
         return info
     else:
         raise Exception('Unknown')
+
 
 if __name__ == '__main__':
     app.run()
